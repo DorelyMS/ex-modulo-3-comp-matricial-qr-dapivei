5f3741d5462b26f6b2bef28eca8cd7a9c28e6659
brunocgf <brunocgf@gmail.com>
2020-04-16 19:53:41 -0500

    agrega funciones de bloques al modulo py unico

--- Codigo/funciones_factorizacion_QR.py
+++ Codigo/funciones_factorizacion_QR.py
@@ -4,7 +4,7 @@
 import numpy as np
 import copy
 import codecs
-
+import sys
 
 
 
@@ -212,3 +212,106 @@ def Q_j(A_r_v,j):
     return Qj
 
 Q_j.__doc__ =busca_ayuda("Q_j")
+
+
+
+
+##### Nota: Solucion_SEL_QR_nxn
+
+
+
+
+def bloques(A, b=False, n1=False, n2=False):
+    """
+    Esta es la función para la creación de bloques usando un arreglo de numpy
+    
+    params: A   Matriz (nxn) que representa los coeficientas de las ecuaciones
+            b   vector (nx1) constantes del sistema
+            n1  Numero de renglones que tendrá el 1er bloque
+            n2  Numero de renglones que tendrá el 2do bloque
+    
+    return: A11 Fraccion de la matriz dividida
+            A12 Fraccion de la matriz dividida
+            A12 Fraccion de la matriz dividida
+            A12 Fraccion de la matriz dividida
+            b1  Fraccion del vector dividido
+            b2  Fraccion del vector dividido
+    """
+
+    # Primero definimos el n
+    m,n = A.shape
+
+    # Condiciones de A
+    # Si no se dan los n deseados, se intentan hacer los bloques casi iguales
+    if  not (n1&n2):
+        n1 = n//2
+        n2 = n - n1
+    # Los bloques deben cumplir la condicion de tamaño
+    elif n1+n1 != n:
+        sys.exit('n1 + n2 debe ser igual a n')
+    else:
+        None
+
+    # Condiciones de b
+    if  b is False:
+        b1 = None
+        b2 = None
+        print('condicion1')
+    elif len(b) == m:
+        b1 = b[:n1]
+        b2 = b[n1:m]
+    else:
+        sys.exit('los renglones de A y b deben ser del mismo tamaño')
+
+    A11 = A[:n1,:n1]
+    A12 = A[:n1,n1:n]
+    A21 = A[n1:m,:n1]
+    A22 = A[n1:m,n1:n]
+
+    return A11,A12,A21,A22,b1,b2
+
+
+
+
+def eliminacion_bloques(A,b):
+    """
+    Función que obtiene la solución de un sistema de ecuaciones lineala (SEL) con n ecuaciones y n incognitas
+            
+    params: A   Matriz (nxn) que representa los coeficientas de las ecuaciones
+            b   vector (nx1) constantes del sistema
+    
+    return: x1 Solucion al 1er sistema de ecuaciones obtenido con la división por bloques
+            x2 Solucion al 2do sistema de ecuaciones obtenido con la división por bloques
+    """
+    if np.linalg.det(A)==0:
+        sys.exit('A debe ser no singular')
+
+    A11,A12,A21,A22,b1,b2 = bloques(A,b)
+
+    if np.linalg.det(A11)==0:
+        sys.exit('A11 debe ser no singular')
+
+    ## 1. Calcular A11^{-1}A12 y A11^{-1}b1 teniendo cuidado en no calcular la inversa sino un sistema de ecuaciones lineales
+    ## Aquí se debe usar el método QR una vez que esté desarrollado
+
+    ## Definimos y = A11^{-1}b1, por tanto A11y=b1. Resolviendo el sistema anterior para 11y:
+    y = Solucion_SEL_QR_nxn(A11,b1)
+    #y = np.linalg.solve(A11,b1)
+
+    ## Definimos Y = A11^{-1}A12
+    Y = Solucion_SEL_QR_nxn(A11,A12)
+    #Y = np.linalg.solve(A11,A12)
+
+    ## 2. Calcular el complemento de Schur del bloque A11 en A. Calcular b_hat
+    S = A22 - A21@Y
+    b_h = b2 - A21@y
+
+    ## 3. Resolver Sx2 = b_hat
+    x2 = Solucion_SEL_QR_nxn(S,b_h)
+    #x2 = np.linalg.solve(S,b_h)
+
+    ## 4. Resolver A11x1 = b1-A12X2
+    x1 = Solucion_SEL_QR_nxn(A11,b1-A12@x2)
+    #x1 = np.linalg.solve(A11,b1-A12@x2)
+
+    return np.concatenate((x1,x2), axis=0)
